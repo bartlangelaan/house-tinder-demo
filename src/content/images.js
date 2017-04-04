74f1cb6c12bb92b703d3ada750a6fbed50041e25
Bart Langelaan <langelaanbart@gmail.com>
2017-04-04 13:48:57 +0200

    Added 7th picture

--- src/content/images.js
+++ src/content/images.js
@@ -6,25 +6,29 @@ const images = [
   {
     image: require('../images/1.jpg'),
     stats: {
-      modern: 1
+      modern: 1,
+      cluttered: 0,
     }
   },
   {
     image: require('../images/2.jpg'),
     stats: {
-      modern: 1
+      modern: 1,
+      cluttered: 0.2
     }
   },
   {
     image: require('../images/3.jpg'),
     stats: {
-      modern: 0
+      modern: 0,
+      cluttered: 0.7
     }
   },
   {
     image: require('../images/4.jpg'),
     stats: {
-      modern: 0
+      modern: 0,
+      cluttered: 0.2
     }
   },
   {
@@ -41,6 +45,13 @@ const images = [
       modern: 0.65
     }
   },
-];
+  {
+    image: require('../images/7.jpg'),
+    stats: {
+      cluttered: 0,
+      modern: 1
+    }
+  },
+].reduce((acc, el) => { acc.push(el); let i = Math.floor(Math.random() * (acc.length)); [acc[i], acc[acc.length - 1]] = [acc[acc.length - 1], acc[i]]; return acc; }, []);
 
 export default images;
